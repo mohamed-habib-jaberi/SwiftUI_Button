94c90ae4acfbdb512f961c1a2e3edd220a6aac05
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-18 01:53:32 +0200

    Add Modifiers to Button

--- SwiftUI_Button/ContentView.swift
+++ SwiftUI_Button/ContentView.swift
@@ -9,8 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let foodPrinter: () -> () = {
+        print("the food was so good! 🐠")
+    }
+    
+    let meow = "Meeow"
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+//            Button("Make Meow"){
+//                print(self.meow)
+//            }
+            Button(action: {
+                
+            }) {
+                VStack{
+                    Text("Give gift")
+                        .font(Font.system(.body))
+                    
+                    Image(systemName: "gift")
+                        .font(Font.system(.largeTitle))
+                    
+                }
+            }
+            
+            Button(action: foodPrinter) {
+                VStack{
+                    Image("Cat")
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .shadow(radius: 10)
+                        .padding()
+                    
+                    Text("Feed kitten!")
+                        .font(Font.system(.headline, design: .rounded))
+                        .foregroundColor(.primary)
+                        .padding([.leading, .trailing] , 10)
+                        .padding([.top,.bottom], 5)
+                        .background(Color.purple)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                    
+                }
+            }
+            
+//            Button(action: {
+//                print("Food was good")
+//            }) {
+//
+//                VStack{
+//                    Image("Cat")
+//                        .renderingMode(.original)
+//                    .resizable()
+//                    .scaledToFit()
+//
+//                     Text("Feed kitten!")
+//                }
+//            }
+            
+        }
     }
 }
 
